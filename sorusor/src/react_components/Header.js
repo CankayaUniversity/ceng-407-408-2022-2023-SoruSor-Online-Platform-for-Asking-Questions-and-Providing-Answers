c3931ec2d2f3fc0eacfbb62e547adbf7f1115b91
cona <berkaycona@outlook.com>
2023-01-30 02:02:43 +0300

    material icon package added / @material bug fixed

--- sorusor/src/react_components/Header.js
+++ sorusor/src/react_components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import HomeIcon from "@mui/icons-material/Home";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="SoruSorHeader">
+      <div className="SoruSorHeaderContent">
+        <div className="SoruSorHeaderLogo">
+          <img src="" />
+          <div className="SoruSorHeaderIcons">
+            <div className="SoruSorHeaderIcon">
+              <HomeIcon />
+            </div>
+            <div className="SoruSorHeaderIcon"></div>
+            <div className="SoruSorHeaderIcon"></div>
+            <div className="SoruSorHeaderIcon"></div>
+            <div className="SoruSorHeaderIcon"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
+//3043
