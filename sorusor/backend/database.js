2d3c5a3675d5ec20d00c0a3fc0d43bc3c5dda5aa
cona <berkaycona@outlook.com>
2023-04-01 19:54:11 +0300

    Published // Backend side models and routes v0.1

---
+++ sorusor/backend/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb://berkaycona:sorusorşiifremongodb12@ac-twh7uei-shard-00-00.srf77lq.mongodb.net:27017,ac-twh7uei-shard-00-01.srf77lq.mongodb.net:27017,ac-twh7uei-shard-00-02.srf77lq.mongodb.net:27017/sorusor-mem?ssl=true&replicaSet=atlas-u43u2z-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB connected succesfully!");
+    })
+    .catch((error) => console.log("Error: ", error));
+};
