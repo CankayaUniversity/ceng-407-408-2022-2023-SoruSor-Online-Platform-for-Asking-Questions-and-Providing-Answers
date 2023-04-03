5c78a4879a8fc17a64259b38710002c6c6521e7b
cona <berkaycona@outlook.com>
2023-04-03 23:48:04 +0300

    Published // bug fix related to throw

--- sorusor/backend/routes/Question.js
+++ sorusor/backend/routes/Question.js
@@ -3,31 +3,33 @@ const router = express.Router();
 
 const questionDB = require("../models/Question");
 
-router.post('/', async (req,res)=>{
-    console.log(req.body)
+router.post("/", async (req, res) => {
+  console.log(req.body);
 
-    try{
-        await questionDB.create({
-            questionName: req.body.questionName,
-            questionUrl:req.body.questionUrl
-        }).then(()=>{
-            res.status(201).send({
-                status:true,
-                message: "Question is added"
-            })
-        }).catch(err)=>{
-            res.status(400).send({
-                status:false,
-                message:"Bad format"
-            })
-        }
-    }catch(e){
-        res.status(500).send({
-            status:false,
-            message:"Error while adding question"
-        })
-    }
-
-})
+  try {
+    await questionDB
+      .create({
+        questionName: req.body.questionName,
+        questionUrl: req.body.questionUrl,
+      })
+      .then(() => {
+        res.status(201).send({
+          status: true,
+          message: "Question is added",
+        });
+      })
+      .catch((err) => {
+        res.status(400).send({
+          status: false,
+          message: "Bad format",
+        });
+      });
+  } catch (e) {
+    res.status(500).send({
+      status: false,
+      message: "Error while adding question",
+    });
+  }
+});
 
 module.exports = router;
