01cd8d8a01c4d4ab9a74940c503d8b27b58f4c20
M.Alkın <mert.alkin2233@hotmail.com>
2023-04-27 14:10:27 +0300

    Post.js error fixed

--- sorusor/frontend/src/react_components/Post.js
+++ sorusor/frontend/src/react_components/Post.js
@@ -5,27 +5,28 @@ import {
   MoreHorizOutlined,
   RepeatOneOutlined,
   ShareOutlined,
-} from "@material-ui/icons";
-import { Avatar } from "@mui/material";
-import React, { useState } from "react";
-import "./css/Post.css";
+} from "@material-ui/icons"; // import icons from Material UI
+import { Avatar } from "@mui/material"; // import Avatar from Material UI
+import React, { useState } from "react"; // import React and useState hook
+import "./css/Post.css"; // import CSS file for Post component
 
-import { Modal } from "react-responsive-modal";
-import "react-responsive-modal/styles.css";
-import CloseIcon from "@material-ui/icons/Close";
+import { Modal } from "react-responsive-modal"; // import Modal component from react-responsive-modal
+import "react-responsive-modal/styles.css"; // import CSS file for react-responsive-modal
+import CloseIcon from "@material-ui/icons/Close"; // import CloseIcon from Material UI
 
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill"; // import ReactQuill
+import "react-quill/dist/quill.snow.css"; // import CSS file for ReactQuill
 
-import ReactTimeAgo from "react-time-ago";
+import ReactTimeAgo from "react-time-ago"; // import ReactTimeAgo component
 
-import axios from "axios";
+import axios from "axios"; // import axios for making HTTP requests
 
-import ReactHtmlParser from "html-react-parser";
-import { useSelector } from "react-redux";
-import { selectUser } from "../feature/userSlice";
+import ReactHtmlParser from "html-react-parser"; // import ReactHtmlParser for parsing HTML tags
+import { useSelector } from "react-redux"; // import useSelector hook from react-redux
+import { selectUser } from "../feature/userSlice"; // import selectUser function from userSlice
 
 function LastSeen({ date }) {
+  // LastSeen component displays the time elapsed since a given date
   return (
     <div>
       <ReactTimeAgo date={date} locale="en-US" timeStyle="round" />
@@ -34,37 +35,37 @@ function LastSeen({ date }) {
 }
 
 function Post({ post }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [answer, setAnswer] = useState("");
-  const Close = <CloseIcon />;
+  const [isModalOpen, setIsModalOpen] = useState(false); // useState hook to manage the state of isModalOpen
+  const [answer, setAnswer] = useState(""); // useState hook to manage the state of answer
+  const Close = <CloseIcon />; // define Close variable as the CloseIcon component from Material UI
 
-  const user = useSelector(selectUser);
+  const user = useSelector(selectUser); // useSelector hook to select user data from the Redux store
 
   const handleQuill = (value) => {
+    // handleQuill function sets the state of answer to the value passed as parameter
     setAnswer(value);
   };
-
-  // console.log(answer);
-
+// console.log(answer);
   const handleSubmit = async () => {
-    if (post?._id && answer !== "") {
+    // handleSubmit function is called when the user submits an answer to a question
+    if (post?._id && answer !== "") { // check if the post ID and answer are valid
       const config = {
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json", // set request header to application/json
         },
       };
       const body = {
         answer: answer,
         questionId: post?._id,
         user: user,
-      };
+      }; // define the body of the POST request
       await axios
-        .post("/api/answers", body, config)
+        .post("/api/answers", body, config) // make a POST request to the server with the answer data
         .then((res) => {
           console.log(res.data);
           alert("Answer added.");
           setIsModalOpen(false);
-          window.location.href = "/";
+          window.location.href = "/"; // redirect to the home page after submitting the answer
         })
         .catch((e) => {
           console.log(e);
